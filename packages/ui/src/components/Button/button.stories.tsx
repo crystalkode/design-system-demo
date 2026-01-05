71361590246a2b95a163ac6ff3a6e7277a20737c
crystalkode <crystal.shih1126@gmail.com>
2026-01-06 07:38:03 +0900

    chore(button): separate color and variant props, add secondary style

--- packages/ui/src/components/Button/button.stories.tsx
+++ packages/ui/src/components/Button/button.stories.tsx
@@ -6,6 +6,8 @@ const meta: Meta<typeof Button> = {
   component: Button,
   tags: ["autodocs"],
   args: {
+    color: "primary",
+    variant:"fill",
     children: "Button",
   },
 };
@@ -14,12 +16,26 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
-  args: { variant: "primary" },
+export const Playground: Story = {};
+
+export const Colors: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: 12 }}>
+      <Button color="primary">Small</Button>
+      <Button color="secondary">Small</Button>
+    </div>
+  ),
 };
 
-export const Secondary: Story = {
-  args: { variant: "secondary" },
+export const Variants: Story = {
+  render: () => (
+    <div style={{ display: "flex", gap: 12 }}>
+      <Button variant="fill">Small</Button>
+      <Button variant="outlined">Small</Button>
+      <Button variant="text">Small</Button>
+
+    </div>
+  ),
 };
 
 export const Disabled = {
@@ -35,6 +51,26 @@ export const Sizes: Story = {
       <Button size="small">Small</Button>
       <Button size="medium">Medium</Button>
       <Button size="large">Large</Button>
+    </div>
+  ),
+};
+
+export const VRT: Story = {
+  parameters: {
+    pseudo: { hover: true },
+  },
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: 12 }}>
+      <div style={{ display: "flex", gap: 12 }}>
+      <Button variant="fill">Button</Button>
+      <Button variant="outlined">Button</Button>
+      <Button variant="text">Button</Button>
+    </div>
+      <div style={{ display: "flex", gap: 12 }}>
+      <Button color="secondary" variant="fill">Button</Button>
+      <Button color="secondary" variant="outlined">Button</Button>
+      <Button color="secondary" variant="text">Button</Button>
+    </div>
     </div>
   ),
 };
