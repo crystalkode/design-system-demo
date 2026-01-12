071b20422fc0a9a5cac9174562e940a7aeca5218
crystalkode <crystal.shih1126@gmail.com>
2026-01-12 19:49:15 +0900

    feat: create vrt stories

--- packages/ui/src/components/Button/button.stories.tsx
+++ packages/ui/src/components/Button/button.stories.tsx
@@ -10,6 +10,9 @@ const meta: Meta<typeof Button> = {
     variant:"fill",
     children: "Button",
   },
+    parameters: {
+    chromatic: { disableSnapshot: true },
+  },
 };
 
 export default meta;
@@ -51,26 +54,6 @@ export const Sizes: Story = {
       <Button size="small">Small</Button>
       <Button size="medium">Medium</Button>
       <Button size="large">Large</Button>
-    </div>
-  ),
-};
-
-export const VRT: Story = {
-  parameters: {
-    pseudo: { hover: true },
-  },
-  render: () => (
-    <div style={{ display: "flex", flexDirection: "column", gap: 12 }}>
-      <div style={{ display: "flex", gap: 12 }}>
-      <Button variant="fill">Button</Button>
-      <Button variant="outlined">Button</Button>
-      <Button variant="text">Button</Button>
-    </div>
-      <div style={{ display: "flex", gap: 12 }}>
-      <Button color="secondary" variant="fill">Button</Button>
-      <Button color="secondary" variant="outlined">Button</Button>
-      <Button color="secondary" variant="text">Button</Button>
-    </div>
     </div>
   ),
 };
