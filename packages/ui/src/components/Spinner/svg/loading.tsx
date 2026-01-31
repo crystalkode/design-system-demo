ea07ba4fd6806e4fa4adf5400fa4ba573145ee26
crystalkode <crystal.shih1126@gmail.com>
2026-01-31 23:01:43 +0900

    feat(button): add loading state

---
+++ packages/ui/src/components/Spinner/svg/loading.tsx
@@ -0,0 +1,12 @@
+export function Loading() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+    </svg>
+  );
+}
